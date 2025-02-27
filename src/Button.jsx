9fd333f7c2ab9fc7892a655a187c959aba068add
sreeja <sreeja.mudili@gmail.com>
2025-02-27 13:03:48 +0530

    created Button component

---
+++ src/Button.jsx
@@ -0,0 +1,20 @@
+//External
+//Module
+//inline
+
+function Button(){
+    const styles={
+            backgroundColor:"blue",
+            color: "white",
+            padding:"10px 20px",
+            borderRadius:"5px",
+    
+    }
+    return(
+
+        <button style={styles}>click me</button>
+
+
+    );
+}
+export default Button;
